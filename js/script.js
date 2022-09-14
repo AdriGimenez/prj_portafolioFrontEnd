343980fbbed671fe258b142b0d460ab35cb34344
AdriGimenez <93161937+AdriGimenez@users.noreply.github.com>
2022-09-13 20:08:08 -0500

    Trabajo terminado

---
+++ js/script.js
@@ -0,0 +1,80 @@
+const ctx = document.getElementById("myChart");
+const myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: [
+        "DB", "Investigacion", "Tiempo", "Ingles", "PP", "T. Equipo",
+    ],
+    datasets: [
+      {
+        label: "Habilidades: Manejo",
+        data: [40, 85, 85, 60, 40, 85],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
+const ctx2 = document.getElementById("myChart2");
+const myChart2 = new Chart(ctx2, {
+  type: "bar",
+  data: {
+    labels: [
+          "HTML", "CSS", "JavaScript", "C/CSS#", "Python", "Java", "Django", "GIT"      
+        ],
+    datasets: [
+      {
+        label: "Lenguajes",
+        data: [70, 60, 40, 65, 85, 85, 65, 70],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
